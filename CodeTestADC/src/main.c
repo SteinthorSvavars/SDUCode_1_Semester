9514c8d2433c1ad73f7f4438a24bb5d634580436
Steinthor Emil Svavarsson <steinthor.emil.svavarsson@gmail.com>
2024-12-10 22:35:44 +0100

    LDRMotionButtonSemiWorking

--- CodeTestADC/src/main.c
+++ CodeTestADC/src/main.c
@@ -20,12 +20,12 @@ int main(void)
   //pwm3_init(); // initialize PWM signal at pin PB0, PB1, PB2 with frequency of 150 Hz
   adc_init(); // initialize the ADC module
 
-  DDRC = 0b11110000; // configure pins PC0 to PC3 as inputs
+  DDRC = 0b11110100; // configure pins PC0 to PC3 as inputs
   DDRD = 0xF0;
   DDRB = 0xF0;
   pinMode(17, INPUT); //MotionSensor
   digitalWrite(17, LOW);
-  PORTC = 0b00110000; // configure pins PC0 to PC3 to not use pullups for the ADC
+  PORTC = 0b00110100; // configure pins PC0 to PC3 to not use pullups for the ADC
   PORTD = 0x00;
   PORTB = 0x00;
  
@@ -40,6 +40,12 @@ int main(void)
     }
     else{
       PORTB &=~_BV(PORTB5);
+    }
+    if(PINC = 0b00111000 && digitalRead(17) == HIGH){
+      PORTD |= _BV(PORTD4);
+    }
+    else{
+      PORTD &=~_BV(PORTD4);
     }
     _delay_ms(100);
   }  
