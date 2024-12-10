5ca79c217ba3f50bb4ac712e06ef151b89a33635
Steinthor Emil Svavarsson <steinthor.emil.svavarsson@gmail.com>
2024-12-10 15:32:16 +0100

    LDRMIx

--- CodeTestADC/src/main.c
+++ CodeTestADC/src/main.c
@@ -17,8 +17,8 @@ int main(void)
   
   uart_init(); 
   io_redirect();
-  //pwm1_init(); // initialize PWM signal at pin PB1, frequency of 4 kHz
-  pwm3_init(); // initialize PWM signal at pin PB0, PB1, PB2 with frequency of 150 Hz
+  pwm1_init(); // initialize PWM signal at pin PB1, frequency of 4 kHz
+  //pwm3_init(); // initialize PWM signal at pin PB0, PB1, PB2 with frequency of 150 Hz
   adc_init(); // initialize the ADC module
 
   DDRC = 0b11110000; // configure pins PC0 to PC3 as inputs
@@ -28,13 +28,14 @@ int main(void)
 
   while(1)
   {
-    for (int i = 1; i < 100; i++){
-      pwm3_set_duty(i, i, i); //duty cycles in percent at PB0, PB1, PB2, from 0 - 100
-      //pwm1_set_duty(i); //duty cycle in percent at PB1, from 0 - 100
-      _delay_ms(100);
-    }
     adc_value = adc_read(0); // Value 0-1023 representing analog voltage on pin PC0
     printf("Result of the ADC conversion : %u\n", adc_value); 
+    //for (int i = 1; i < 100; i++){
+      //pwm3_set_duty(i, i, i); //duty cycles in percent at PB0, PB1, PB2, from 0 - 100
+      pwm1_set_duty(adc_value/10); //duty cycle in percent at PB1, from 0 - 100
+      _delay_ms(10);
+   // }
+    
 
     
   }
