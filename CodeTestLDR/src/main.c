ec91cae1ab666d99e487ff24e4a5aed50f2ce09d
Steinthor Emil Svavarsson <steinthor.emil.svavarsson@gmail.com>
2024-12-10 14:30:54 +0100

    LDRSemiWorkin

--- CodeTestLDR/src/main.c
+++ CodeTestLDR/src/main.c
@@ -1,33 +1,47 @@
-/*
- * HelloWorld.c
- *
- * Created: 11/9/2023 10:43:27 AM
- * Author : Alin
- */ 
-
-
-
 #include <stdio.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include <Arduino.h> //set framework to arduino 
 
 #include "usart.h"
 
-int main(void) {  
+//const int SensorPin = A6;
+const int LEDPin = 2;
 
-  int input;  
+int SensorMin = 1023; //LDR min value
+int SensorMax = 0; //LDR max value
+int SensorValue = 0;
+
+int main(void) {  
+  pinMode(A6, INPUT);
+  pinMode(13, OUTPUT);
+  pinMode(LEDPin, OUTPUT);
+  digitalWrite(13, HIGH);
+  digitalWrite(LEDPin, LOW);
+  //analogWrite(A6, LOW);
+  
 
   uart_init(); // open the communication to the microcontroller
   io_redirect(); // redirect input and output to the communication
 
-    
-  while(1) {
-		
-	  printf("Type in a number \n");
-    scanf("%d", &input);
-    printf("The number you typed is %d is %x in hexadecimal \n", input, input);
-	  _delay_ms(1000)	;
+  while(millis() < 5000){
+    SensorValue = analogRead(A6);
+    if(SensorValue > SensorMax){
+      SensorMax = SensorValue;
+    }
+    if(SensorValue < SensorMin){
+      SensorMin = SensorValue;
+    }
+    digitalWrite(13, LOW);
+  }
 
+  while(1) {
+		SensorValue = analogRead(A6);
+    printf("%d", SensorValue);
+    //_delay_ms(550);
+    SensorValue = constrain(SensorValue, SensorMin, SensorMax);
+    //SensorValue = map(SensorValue, SensorMin, SensorMax, 0, 255);
+    analogWrite(LEDPin, SensorValue/5);
   }
   
   return 0;
