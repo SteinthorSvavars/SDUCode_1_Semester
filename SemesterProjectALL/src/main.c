ddf242b244b63e9afe38d725d0bdc89d997e5e1d
Steinthor Emil Svavarsson <steinthor.emil.svavarsson@gmail.com>
2024-12-19 04:19:13 +0100

    SemesterAllCodeSecond

--- SemesterProjectALL/src/main.c
+++ SemesterProjectALL/src/main.c
@@ -16,10 +16,21 @@
 #include "i2cmaster.h"
 #include "lcd.h"
 
+/*** Global ***/
+int STOP;
 /*** Entrance ***/
 int LEDENT; int LEDOUT; unsigned int Buttons; unsigned int LDR;
+/*** Hallway ***/
+int LEDHALL;
+/*** Bedroom ***/
 
-void ENT(Void){
+int ModeSelect(void){
+
+}
+
+void ENT(Void){   //Entrence function
+int EntranceOn = 1;
+do{
   Buttons = adc_read(6);  // Value 0-1023 representing analog voltage on pin PC6
     _delay_ms(100);
     if(Buttons >= 570){ // When adc read is over 569 entrance light turns on or off
@@ -63,32 +74,135 @@ void ENT(Void){
         PORTC &= ~_BV(PORTC3);
       }
       _delay_ms(100);
-    } 
+    }
+    if((PIND & (1<<2)) == 0){
+      EntranceOn = 0;
+    }
+  }while(EntranceOn == 1);
+}
+
+void HallWay(Void){   //Hallway funtion
+  int HallWayOn = 1;
+  do{
+   if((PIND & (1 << 7)) == 0){
+      _delay_ms(150);
+      if(LEDHALL == 0){
+        PORTD |= _BV(PORTD4);
+      }
+      else if(LEDHALL == 1){
+        PORTD &= ~_BV(PORTD4);
+      }
+      if(LEDHALL == 0){
+        LEDHALL = 1;
+      }
+      else if(LEDHALL == 1){
+        LEDHALL = 0;
+      }
+    }
+    else if((PIND & (1<<2)) == 0){
+      HallWayOn = 0;
+    }
+  }while(HallWayOn == 1);
+}
+
+void BedRoom(void){   // Bedroom function
+  unsigned int BedRoomPWM; int a; int BedRoomOn = 1;
+  do{
+    if ((PIND & (1 << 2)) == 0)
+      {
+        _delay_ms(150);
+        if (a == 1) {
+          while (a == 1) {
+            BedRoomPWM = adc_read(0);
+            pwm3_set_duty(BedRoomPWM/4);
+            if ((PIND & (1 << 2)) == 0) {
+              _delay_ms(150);
+              PORTD &= ~_BV(PORTD3);
+              a = 0;
+            }
+            else if((PIND & (1<<2)) == 0){
+              BedRoomOn = 0;
+            }
+          }
+        }
+      }
+    else {
+      PORTD &= ~_BV(PORTD3);
+    }
+  }while(BedRoomOn == 1);
 }
 
 int main(void) { 
 
   uart_init(); // open the communication to the microcontroller
-  io_redirect(); // redirect input and output to the communication  
+  //io_redirect(); // redirect input and output to the communication
+  i2c_init(); 
+  LCD_init(); 
   adc_init(); // initialize the ADC module 
+  pwm3_init();
+  adc_init();
 
-  DDRC = 0x0C;  //Pins 0-1 4-7 output / Pins 2&3 input
-  
-  PORTC = 0x00; //Pins 4-7 Pullup / Pins 0-3 floating
+  /*** PORTB ***/
+  DDRB |= _BV(DDB0);  // PORTB 0  Hallway Light : D8
+  DDRB |= _BV(DDB1);  // PORTB 1  Poul RGB  : D9
+  DDRB |= _BV(DDB2);  // PORTB 2  Poul RGB  : D10
+  DDRB |= _BV(DDB3);  // PORTB 3  Poul RGB  : D11
+  DDRB &=~_BV(DDB4);  // PORTB 4  Motion Sensor : D12
+  DDRB |= _BV(DDB5);  // PORTB 5  : Motion LDR controller : D13
+  /*** PORTC ***/
+  DDRC |= _BV(DDC0);  // PORTC 0  Emilija PWM : A0
+  DDRC |= _BV(DDC1);  // PORTC 1  Poul  PWM : A1
+  DDRC |= _BV(DDC2);  // PORTC 2  : Entrance light  : A2
+  DDRC |= _BV(DDC3);  // PORTC 3  : Outside light   : A3
+  //DDRC |= _BV(DDC4);  // PORTC 4  LCD : A4
+  //DDRC |= _BV(DDC5);  // PORTC 5  LCD : A5
+  //DDRC &= ~_BV(DDC6);  // PORTC 6  Button ENT / OUT: A6  : Analog only
+  //DDRC &= ~_BV(DDC7);  // PORTC 7  LDR : A7              : Analog only
+  /*** PORTD  ***/
+  DDRD |=_BV(DDD0);  // PORTD 0  Rx  : D0
+  DDRD |=_BV(DDD1);  // PORTD 1  Tx  : D1
+  DDRD &=~_BV(DDD2);  // PORTD 2  Emilija Button : D2
+  DDRD &=~_BV(DDD3);  // PORTD 3  Emilija Light  : D3
+  DDRD &=~_BV(DDD4);  // PORTD 4  Poul  : D4
+  DDRD &=~_BV(DDD5);  // PORTD 5  Select / Stop: D5
+  DDRD &=~_BV(DDD6);  // PORTD 6  Next / Lights on/off: D6
+  DDRD &=~_BV(DDD7);  // PORTD 7 = input  : Hallway button  : D7
 
-  DDRB |= _BV(DDB5);
-  DDRC |= _BV(DDC2);
-  DDRC |= _BV(DDC3);
-  DDRB &=~_BV(DDB4);
+  /*** B ***/
+  PORTB &=~ _BV(DDB0);
+  PORTB &=~ _BV(DDB1);
+  PORTB &=~ _BV(DDB2);
+  PORTB &=~ _BV(DDB3);
+  PORTB &=~ _BV(DDB4);
+  PORTB &=~ _BV(DDB5);
 
-  int input;  
+  /*** C ***/
+  PORTC = _BV(DDC0);
+  PORTC = _BV(DDC1);
+  PORTC = _BV(DDC2);
+  PORTC = _BV(DDC3);
+  //PORTC = _BV(DDC4);
+  //PORTC &=~ _BV(DDC5);
 
-  uart_init(); // open the communication to the microcontroller
-  io_redirect(); // redirect input and output to the communication
-
+  /*** D ***/
+  PORTD |= _BV(DDD0);
+  PORTD |= _BV(DDD1);
+  PORTD = _BV(DDD2);
+  PORTD = _BV(DDD3);
+  PORTD = _BV(DDD4);
+  PORTD = _BV(DDD5);
+  PORTD = _BV(DDD6);
+  PORTD &=~ _BV(DDD7);
     
   while(1) {
-		
+    LCD_set_cursor(0,0);
+    printf("ENT");
+    LCD_set_cursor(0,1);
+    printf("Hall");
+    LCD_set_cursor(0,2);
+    printf("Bed");
+    LCD_set_Cursor(0,3);
+    printf("Liv");
   }
   
   return 0;
